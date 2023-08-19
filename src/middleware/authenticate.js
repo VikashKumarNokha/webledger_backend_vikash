8977dba16deea746b05714e79fc3977598a700b5
Vikash Kumar <vikashkumarnokha@gmail.com>
2023-08-19 23:14:11 +0530

    authenication done

---
+++ src/middleware/authenticate.js
@@ -0,0 +1,52 @@
+
+const jwt = require("jsonwebtoken")
+
+require("dotenv").config()
+
+ const verifyTocken = (token) =>{
+         return new Promise (( reject, resolve)=>{
+        var decodedResult =  jwt.verify(token, process.env.secrete_key, function(err, decoded) {
+
+              if(err){
+              return  reject( err )
+              }
+             return resolve( decoded) 
+
+            });
+         } )
+ }
+
+const authenticate = async (req, res, next) =>{
+
+      if(!req.headers.authorization){
+         return res.status(400).send({message : "Autentication tocken not found or invalid"});
+      }
+
+      if(!req.headers.authorization.startsWith("Bearer")){
+          return res.status(400).send({message : "Autentication tocken not found or invalid"})
+      }
+
+      const tocken = req.headers.authorization.trim().split(" ")[1];
+
+      let decoded 
+
+       try{
+
+         // decoded = await verifyTocken(tocken);
+          decoded =   jwt.verify(tocken, "masaisecret", function(err, decoded) {
+              if(err){
+                return false
+              }
+              return decoded
+            });
+          req.user =  decoded.newuser
+
+       }catch(err){
+          return res.status(400).send({err})
+       }    
+   
+      return next()
+}
+
+
+module.exports =  authenticate ;
